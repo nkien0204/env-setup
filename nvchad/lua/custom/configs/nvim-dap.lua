e50ec4eaaf3a4ddd05c563bfc1f6d02f21e8e71a
ntkien <ntkien11@cmcglobal.vn>
2023-12-04 20:53:48 +0700

    update go dap

--- nvchad/lua/custom/configs/nvim-dap.lua
+++ nvchad/lua/custom/configs/nvim-dap.lua
@@ -3,37 +3,6 @@ local dap = require "dap"
 vim.fn.sign_define("DapBreakpoint", { text = "⬢", texthl = "Yellow", linehl = "", numhl = "Yellow" })
 vim.fn.sign_define("DapStopped", { text = "▶", texthl = "Green", linehl = "ColorColumn", numhl = "Green" })
 
-dap.adapters.go = function(callback, config)
-  local stdout = vim.loop.new_pipe(false)
-  local handle
-  local pid_or_err
-  local port = 38697
-  local opts = {
-    stdio = { nil, stdout },
-    args = { "dap", "-l", "127.0.0.1:" .. port },
-    detached = true,
-  }
-  handle, pid_or_err = vim.loop.spawn("dlv", opts, function(code)
-    stdout:close()
-    handle:close()
-    if code ~= 0 then
-      print("dlv exited with code", code)
-    end
-  end)
-  assert(handle, "Error running dlv: " .. tostring(pid_or_err))
-  stdout:read_start(function(err, chunk)
-    assert(not err, err)
-    if chunk then
-      vim.schedule(function()
-        require("dap.repl").append(chunk)
-      end)
-    end
-  end)
-  vim.defer_fn(function()
-    callback { type = "server", host = "127.0.0.1", port = port }
-  end, 100)
-end
-
 dap.adapters.delve = {
   type = "server",
   port = "${port}",
@@ -42,6 +11,8 @@ dap.adapters.delve = {
     args = { "dap", "-l", "127.0.0.1:${port}" },
   },
 }
+
+dap.adapters.go = dap.adapters.delve
 
 dap.adapters.codelldb = {
   type = "server",
