b418b5d5633cd21eca9fafa0c264783107c6ac3a
ntkien <ntkien11@cmcglobal.vn>
2023-12-15 21:39:51 +0700

    update custom

--- nvchad/lua/custom/chadrc.lua
+++
@@ -1,8 +0,0 @@
----@type ChadrcConfig
-local M = {}
-
-M.ui = { theme = 'onedark' }
-M.mappings = require "custom.mappings"
-M.plugins = "custom.plugins"
-
-return M
--- nvchad/lua/custom/configs/toggleterm.lua
+++
@@ -1,4 +0,0 @@
-local toggle = require "toggleterm"
-
-toggle.setup {}
-return toggle
